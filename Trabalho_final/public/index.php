f6ee78dc7709144cc322c71ddfa4c0d056172de4
MarceloSchwartz <marceloschwartz71@gmail.com>
2025-12-03 22:50:51 -0300

    Update index.php

--- Trabalho_final/public/index.php
+++ Trabalho_final/public/index.php
@@ -16,7 +16,7 @@ require_once('src/perguntas.php');
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pergunta_titulo; ?> (<?php echo $numero_pergunta; ?>/<?php echo $total_perguntas; ?>)</title>
-    <link rel="stylesheet" href="style.css"> 
+    <link rel="stylesheet" href="css/style.css"> 
 </head>
 <body>
     <div class="container">
